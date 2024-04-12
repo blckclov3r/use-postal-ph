8575aa51d5e102e3ee566204812cb13594fba5e1
blckclov3r <blckclov3r@gmail.com>
2024-04-13 04:08:46 +0800

    Squashed commit of the following:
    commit cf1dd859c954caec55bc0ab617afdc40fbbbecce
    Author: blckclov3r <blckclov3r@gmail.com>
    Date:   Sat Apr 13 04:07:05 2024 +0800
    
        playwright test

---
+++ tests/web.spec.ts
@@ -0,0 +1,59 @@
+import {expect, Page, test} from '@playwright/test';
+
+const baseUrl = 'https://use-postal-ph.vercel.app/';
+
+async function setup(page: Page) {
+    await page.goto(baseUrl);
+    await page.waitForLoadState('domcontentloaded');
+    await expect(page).toHaveTitle(/use-postal-ph/);
+    const buttons = ['Main', 'Municipality', 'Region', 'Location', 'Post Code'];
+    for (const buttonName of buttons) {
+        const button = await page.getByRole('button', {name: buttonName});
+        await expect(button).toBeVisible();
+    }
+}
+
+async function clickButton(page: Page, buttonName: string) {
+    await page.getByRole('button', {name: buttonName}).click();
+}
+
+async function selectOptionAndSubmit(page: Page, comboboxName: string, optionName: string) {
+    const combobox = await page.getByRole('combobox', {name: comboboxName});
+    await combobox.type(optionName);
+    await page.getByRole('option', {name: optionName, exact: true}).click();
+    await page.locator('.MuiBox-root > button').first().click();
+}
+
+async function clearInput(page: Page, comboboxName: string) {
+    await page.getByRole('combobox', {name: comboboxName}).click();
+    await page.getByLabel('Clear').click();
+    await page.locator('.MuiBox-root > button').first().click();
+}
+
+test('main-municipality', async ({page}) => {
+    await setup(page);
+    await clickButton(page, 'Main');
+    await selectOptionAndSubmit(page, 'municipality', 'Talisay');
+    await clearInput(page, 'municipality');
+});
+
+test('main-region', async ({page}) => {
+    await setup(page);
+    await clickButton(page, 'Main');
+    await selectOptionAndSubmit(page, 'region', 'VII');
+    await clearInput(page, 'region');
+});
+
+test('main-location', async ({page}) => {
+    await setup(page);
+    await clickButton(page, 'Main');
+    await selectOptionAndSubmit(page, 'location', 'Cebu');
+    await clearInput(page, 'location');
+});
+
+test('main-postcode', async ({page}) => {
+    await setup(page);
+    await clickButton(page, 'Main');
+    await selectOptionAndSubmit(page, 'post_code', '6045');
+    await clearInput(page, 'post_code');
+});
